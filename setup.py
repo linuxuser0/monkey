04904599b5eac8bb2326bcfa5e0ca10a248f2a56
Linux User <sudormdashrfslash@gmail.com>
2013-11-29 12:53:00 -0800

    Add LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Monkey',
+    version='1.0',
+    packages=['monkey'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
---
+++ test/test.py
@@ -0,0 +1,16 @@
+import unittest
+import monkey.corpus
+
+class TestMonkey(unittest.TestCase):
+
+    def setUp(self):
+        self.corpus = corpus.Corpus()
+
+    def test_corpus_init(self):
+        pass
+
+    def test_corpus_next_image(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
