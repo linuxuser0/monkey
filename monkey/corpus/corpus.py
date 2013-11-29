c7b6e6641b147dfe83598dfdeecdf2ad1812d028
Linux User <sudormdashrfslash@gmail.com>
2013-11-29 15:45:28 -0800

    Update corpus.py for new dir structure

--- monkey/corpus/corpus.py
+++ monkey/corpus/corpus.py
@@ -3,47 +3,51 @@ import shutil
 import random
 
 class Corpus: 
-	def __init__(self, name, percent):
-		self.name = name
-		self.cd = os.path.dirname(os.path.realpath(__file__))
-		self.imagedir = os.path.join(self.cd, "corpus")
-		self.newimagedir = os.path.join(self.cd, self.name, "data")	
+    def __init__(self, name, percent):
+        self.name = name
+        self.cd = os.path.dirname(os.path.realpath(__file__))
+        self.imagedir = os.path.join(self.cd, "data")
+        self.newimagedir = os.path.join(os.path.split(self.cd)[0], self.name, "data")    
+        
+                
+        # Create and rework paths.
+        
+        if os.path.isdir(self.newimagedir):
+            shutil.rmtree(self.newimagedir)
+            os.makedirs(self.newimagedir)            
+    
+        for root, dirs, files in os.walk(self.imagedir):
+            for subdir in dirs:
+                os.makedirs(os.path.join(self.newimagedir, subdir))
 
-		# Create and rework paths.
-		
-		if os.path.isdir(self.newimagedir):
-			shutil.rmtree(self.newimagedir)
-			os.makedirs(self.newimagedir)			
-	
-		for root, dirs, files in os.walk(self.imagedir):
-			for subdir in dirs:
-				os.makedirs(os.path.join(self.newimagedir, subdir))
+        # Now add the images.
 
-		# Now add the images.
+        self.images = []
+        self.unused = {} 
+        for subdir in os.listdir(self.imagedir):
+            fullpath = os.path.join(self.imagedir, subdir)
+            self.images = os.listdir(fullpath)
+            self.unused[subdir] = os.listdir(fullpath)    
+            for i in range(0, int(len(self.images)*percent/100.0)):
+                image = random.choice(self.images)
+                self.images.remove(image)
+                self.unused[subdir].remove(image)
+                filepath = os.path.join(self.imagedir, subdir, image) 
+                target = os.path.join(self.newimagedir, subdir)
+                print(filepath, target, "\n")
+                shutil.copy(filepath, target)    
+                
+        print(self.newimagedir)
 
-		self.images = []
-		self.unused = {} 
-		for subdir in os.listdir(self.imagedir):
-			fullpath = os.path.join(self.imagedir, subdir)
-			self.images = os.listdir(fullpath)
-			self.unused[subdir] = os.listdir(fullpath)	
-			for i in range(0, int(len(self.images)*percent/100.0)):
-				image = random.choice(self.images)
-				self.images.remove(image)
-				self.unused[subdir].remove(image)
-				filepath = os.path.join(self.imagedir, subdir, image) 
-				target = os.path.join(self.newimagedir, subdir)
-				print(filepath, target, "\n")
-				shutil.copy(filepath, target)	
-		
+        
 
-		
+        
 
-	def getNextImage(self, name):
-		item = random.choice(self.unused[name])
-		self.unused[name].remove(item)
-		fullpath = os.path.join(self.imagedir, name, item)
-		target = os.path.join(self.newimagedir, name)
-		shutil.copy(fullpath, target)
-		return fullpath	
-	
+    def getNextImage(self, name):
+        item = random.choice(self.unused[name])
+        self.unused[name].remove(item)
+        fullpath = os.path.join(self.imagedir, name, item)
+        target = os.path.join(self.newimagedir, name)
+        shutil.copy(fullpath, target)
+        return fullpath    
+    
