84ead8b9c0e1063e95c40cfffc7e134087f94d61
Linux User <sudormdashrfslash@gmail.com>
2013-12-12 20:28:26 -0800

    Make messy code

--- monkey/helpers/cell_helper.py
+++ monkey/helpers/cell_helper.py
@@ -1,11 +1,12 @@
 import os
 import shutil
+import random
 import glimpse.experiment
 import glimpse.models
 
-class CellHelper():
+class CellHelper(): # TODO - adapt for hybrid (un)superv.
     
-    def __init__(self, path, count, corpus, add_count=1, init_count=2): 
+    def __init__(self, path, count, corpus, add_count=1, init_count=2, prototypes=None, layer="C2"): 
         self.path = path
         self.count = count # number of S2s
         self.exp = glimpse.experiment.ExperimentData()
@@ -13,6 +14,16 @@ class CellHelper():
         self.corpus = corpus
         self.corpus_path = os.path.join(self.path, "cell")
         self.exp.extractor.model = glimpse.models.MakeModel()
+        self.layer = layer
+        self.pool = glimpse.pools.MakePool()
+        self.add_count = add_count
+        self.init_count = init_count
+        self.prototypes = prototypes
+        if self.prototypes is not None:
+            self.supervised = False
+        else:
+            self.supervised = True
+
         print self.corpus
 
     def imprint(self):
@@ -21,7 +32,7 @@ class CellHelper():
         return [glimpse.experiment.GetPrototype(self.exp, n) for n in range(
             glimpse.experiment.GetNumPrototypes(self.exp))]
 
-    def get_next_images(self):
+    def get_next_images(self): # FIX TO SIMULATE FEED
         self.reset_cell()
         self.populate_cell()
         self.categorize_images()
@@ -38,10 +49,26 @@ class CellHelper():
         try:
             os.makedirs(self.celldir)
             for s in self.corpus.subdirs:
-                path = os.path.join(self.celldir, s) os.makedirs(path)
+                path = os.path.join(self.celldir, s) 
+                os.makedirs(path)
 
         except Exception:
             print "Could not create folder"
+
+    def setup_temp(self): # fix this to above
+        try:
+            shutil.rmtree(self.temp_path)
+        except Exception:
+            pass
+
+        try:
+            os.makedirs(self.temp_path)
+            for s in self.corpus.subdirs:
+                path = os.path.join(self.temp_path, s)
+                os.makedirs(path)
+        except Exception:
+            print "Failure in setup_temp"
+            
 
     def cell_check(self):
         try:
@@ -77,11 +104,17 @@ class CellHelper():
                 target = os.path.join(self.celldir, s)
                 shutil.copy(fullpath, target)
 
-    def add_images(self, count=self.add_count): 
+    def add_images(self, count=None): 
+        if count is None:
+            count = self.add_count
         for n in xrange(count):
             name = random.choice(self.corpus.subdirs)
-            folder = os.path.join(corpus.imagedir, name)
+            folder = os.path.join(self.corpus.imagedir, name)
             item = random.choice(os.listdir(folder)) # MAKE UNUSED
             fullpath = os.path.join(folder, item)
-            target = os.path.join(self.corpus, name)
+            target = os.path.join(self.corpus_path, name)
             shutil.copy(fullpath, target)
+
+    def get_categories(self):
+       self.imprint()
+
